239ce74bc26232e34da86c0c6d7a9f4e6d2545c4
ayame <144100281+Ayame918@users.noreply.github.com>
2023-12-12 17:47:08 +0000

    ある程度のルーティング＋Topの大まかなレイアウト

--- app/models/post_bookmark.rb
+++ app/models/post_bookmark.rb
@@ -1,2 +1,4 @@
 class PostBookmark < ApplicationRecord
+  has_many :post
+  has_many :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,73 +1,56 @@
 Rails.application.routes.draw do
+  
+  devise_for :users,skip: [:passwords],controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  devise_for :admins, controllers: {
+    sessions: "admin/sessions"
+  }
+  
   resources :videos
+  scope module: :public do
+    root to: 'homes#top'
+    
+    get 'relationship/follower'
+    get 'relationship/followed'
+    
+    resources :followings, only: [:create, :destroy]
+    resources :tags, only: [:index, :create]
+    resources :bookmarks, only: [:index, :create, :destroy]
+    resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
+    resources :posts, only: [:show, :create, :destroy]
+    
+    resources :users, only: [:index, :show, :edit, :update] do
+      get "mypage" => "users#mypage", on: :member
+      get "withdraw" => "users#withdraw", on: :member
+      resource :relationships, only: [:create, :destroy]
+      get "followings" => "relationships#followings", as: "followings"
+      get "followers" => "relationships#followers", as: "followers"
+    end
+    
+    resource :session, only: [:new, :create, :destroy]
+    resource :registrations, only: [:new, :create]
+  end
+  
   namespace :admin do
     get 'users/index'
     get 'users/show'
     get 'users/edit'
     get 'users/update'
-  end
-  namespace :admin do
+    
     get 'tags/index'
     get 'tags/edit'
     get 'tags/update'
-  end
-  namespace :admin do
+    
     get 'posts/index'
     get 'posts/show'
-  end
-  namespace :admin do
+    
     get 'homes/top'
+    
+    resource :sessions, only: [:new, :create, :destroy]
   end
-  namespace :public do
-    get 'relationship/follower'
-    get 'relationship/followed'
-  end
-  namespace :public do
-    get 'followings/create'
-    get 'followings/destroy'
-  end
-  namespace :public do
-    get 'tags/index'
-    get 'tags/create'
-  end
-  namespace :public do
-    get 'bookmarks/index'
-    get 'bookmarks/create'
-    get 'bookmarks/destroy'
-  end
-  namespace :public do
-    get 'comments/create'
-    get 'comments/destroy'
-  end
-  namespace :public do
-    get 'favorites/create'
-    get 'favorites/destroy'
-  end
-  namespace :public do
-    get 'posts/new'
-    get 'posts/show'
-    get 'posts/destroy'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/mypage'
-    get 'users/withdraw'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'registrations/new'
-    get 'registrations/create'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
-  devise_for :users
-  devise_for :admins
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
