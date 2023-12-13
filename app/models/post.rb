2ae9c50909fea1819c0c3f40ccbe8911932ddd6b
ayame <144100281+Ayame918@users.noreply.github.com>
2023-12-13 18:46:08 +0000

    投稿機能追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
   has_many :user
+  has_one_attached :image
+  has_one_attached :video
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users,skip: [:passwords],controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -7,21 +7,21 @@ Rails.application.routes.draw do
   devise_for :admins, controllers: {
     sessions: "admin/sessions"
   }
-  
+
   resources :videos
   scope module: :public do
     root to: 'homes#top'
-    
+
     get 'relationship/follower'
     get 'relationship/followed'
-    
+
     resources :followings, only: [:create, :destroy]
     resources :tags, only: [:index, :create]
     resources :bookmarks, only: [:index, :create, :destroy]
     resources :comments, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
-    resources :posts, only: [:show, :create, :destroy]
-    
+    resources :posts, only: [:new, :index, :show, :create, :edit, :update, :destroy]
+
     resources :users, only: [:index, :show, :edit, :update] do
       get "mypage" => "users#mypage", on: :member
       get "withdraw" => "users#withdraw", on: :member
@@ -29,26 +29,26 @@ Rails.application.routes.draw do
       get "followings" => "relationships#followings", as: "followings"
       get "followers" => "relationships#followers", as: "followers"
     end
-    
+
     resource :session, only: [:new, :create, :destroy]
     resource :registrations, only: [:new, :create]
   end
-  
+
   namespace :admin do
     get 'users/index'
     get 'users/show'
     get 'users/edit'
     get 'users/update'
-    
+
     get 'tags/index'
     get 'tags/edit'
     get 'tags/update'
-    
+
     get 'posts/index'
     get 'posts/show'
-    
+
     get 'homes/top'
-    
+
     resource :sessions, only: [:new, :create, :destroy]
   end
 
